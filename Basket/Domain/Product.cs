ae868788a2127df635b9d4ac982ab5f1da954459
panververis <panververis@gmail.com>
2018-09-09 12:37:27 +0100

    Added my "Product" domain class

---
+++ Basket/Domain/Product.cs
@@ -0,0 +1,31 @@
+﻿using Basket.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Domain
+{
+    public class Product
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        public Product(string name, decimal price, ProductTypes productType) {
+            Name        = name;
+            Price       = price;
+            ProductType = productType;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string       Name        { get; private set; }
+        public decimal      Price       { get; private set; }
+        public ProductTypes ProductType { get; private set; }
+
+        #endregion
+    }
+}
