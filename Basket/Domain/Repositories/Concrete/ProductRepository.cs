044b4d5c39e31a695d2a58371aa9a0eb91018cab
panververis <panververis@gmail.com>
2018-09-09 16:38:26 +0100

    Refactored my ProductRepository, now have 4 passing Unit tests. Lets fix the PromoRepository now!

--- Basket/Domain/Repositories/Concrete/ProductRepository.cs
+++ Basket/Domain/Repositories/Concrete/ProductRepository.cs
@@ -6,19 +6,19 @@ namespace Basket.Domain.Repositories.Concrete
 {
     public class ProductRepository : IProductRepository
     {
-        public Product GetBread()
-        {
-            throw new NotImplementedException();
-        }
-
         public Product GetButter()
         {
-            throw new NotImplementedException();
+            return new Product("Super awesome butter brand", 0.80m, Common.Enums.ProductTypes.Butter);
         }
 
         public Product GetMilk()
         {
-            throw new NotImplementedException();
+            return new Product("Super awesome milk brand", 1.15m, Common.Enums.ProductTypes.Milk);
+        }
+
+        public Product GetBread()
+        {
+            return new Product("Super awesome bread brand", 1.00m, Common.Enums.ProductTypes.Bread);
         }
 
         public IEnumerable<Product> GetProducts(int? buttersQty, int? milksQty, int? breadsQty)
