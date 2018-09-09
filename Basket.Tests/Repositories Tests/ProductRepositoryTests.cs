90cb6eed518c513c2829bcb8cb925c604e33a267
panververis <panververis@gmail.com>
2018-09-09 15:43:46 +0100

    Added two new (incomplete) Unit Tests, shaping the form of: 1.) my Repos, as they will hold the methods defined here 2.) my "Common" project, that will just hold the shape of the required "Test" data (promos and prices)

---
+++ Basket.Tests/Repositories Tests/ProductRepositoryTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Basket.Tests.Repositories_Tests
+{
+    public class ProductRepositoryTests
+    {
+        #region Tests
+
+        [Fact]
+        public void GetButterShouldReturnPredefinedButterProduct() {
+            //  Arrange
+            //ProductRepository prodRepo = new prodRepo();
+
+            //  Act
+            //Product butter = prodRepo.GetButter();
+
+            //  Assert
+            //Assert.Equal(IsTestButter(butter), true);
+        }
+
+        [Fact]
+        public void GetMilkShouldReturnPredefinedMilkProduct() {
+            //  Arrange
+            //ProductRepository prodRepo = new prodRepo();
+
+            //  Act
+            //Product milk = prodRepo.GetMilk();
+
+            //  Assert
+            //Assert.Equal(IsTestMilk(milk), true);
+        }
+
+        [Fact]
+        public void GetBreadShouldReturnPredefinedBreadProduct() {
+            //  Arrange
+            //ProductRepository prodRepo = new prodRepo();
+
+            //  Act
+            //Product bread = prodRepo.GetBread();
+
+            //  Assert
+            ////Assert.Equal(IsTestBread(bread), true);
+        }
+
+        [Theory]
+        [InlineData(null, null, null)]
+        public void GetProductsWithCorrectNumberOfProducts(int? buttersQty, int? milksQty, int? breadQty) {
+            //  Arrange
+            //ProductRepository prodRepo = new prodRepo();
+
+            //  Act
+            //IEnumerable products = prodRepo.GetProducts();
+
+            //  Assert
+            //Assert that the returned IEnumerable of Products holds the requested quantity of products of each kind
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        //  Here I will reference a common place in code that will define what are the "predefined" Products,
+        //  as required for the needs of this small project
+        //  These will include  "IsTestButter", "IsTestMilk", "IsTestBread" for example
+
+
+        #endregion
+    }
+}
---
+++ Basket.Tests/Repositories Tests/PromoRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Basket.Tests.Repositories_Tests
+{
+    public class PromoRepositoryTests
+    {
+        #region Tests
+
+        [Fact]
+        public void GetPromoOneShouldReturnProperPromo()
+        {
+            //  Arrange
+            //PromoRepository promoRepo = new promoRepo();
+
+            //  Act
+            //Promo promoOne = promoRepo.GetPromoOne();
+
+            //  Assert
+            //Assert that the returned promo is the "predefined" first promo, 
+            //suitable for the purposes of this project
+        }
+
+        [Fact]
+        public void GetPromoTwoShouldReturnProperPromo()
+        {
+            //  Arrange
+            //PromoRepository promoRepo = new promoRepo();
+
+            //  Act
+            //Promo promoTwo = promoRepo.GetPromoTwo();
+
+            //  Assert
+            //Assert that the returned promo is the "predefined" second promo, 
+            //suitable for the purposes of this project
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        //  Here I will reference a common place in code that will define what are the "predefined" Promos,
+        //  as required for the needs of this small project
+        //  These will include  "IsTestPromoOne", "IsTestPromoTwo" for example
+
+        #endregion
+    }
+}
