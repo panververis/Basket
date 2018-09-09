0d346153dba49ec49fcaf7996d2b4ffd4a2fb38e
panververis <panververis@gmail.com>
2018-09-09 12:07:14 +0100

    Added an enum that holds the different types of Products

---
+++ Basket/Enums/ProductTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Enums
+{
+    public enum ProductTypes
+    {
+        Butter  = 1,
+        Milk    = 2,
+        Bread   = 3
+    }
+}
